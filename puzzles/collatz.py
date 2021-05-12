9c75b1038489068305e586c8e0142a051438eb9b
Graeme McKerrell <31652935+gmckerrell@users.noreply.github.com>
2021-05-12 11:11:26 +0100

    Update collatz.py

--- puzzles/collatz.py
+++ puzzles/collatz.py
@@ -50,10 +50,31 @@ def getLargest(maxValue):
             largest = result
     return largest
 
-# test some values
-for i in (10,78,231,1897):
-    print(collatz(i))
+if __name__ == '__main__':
+    import unittest
 
-# find largest sequence
-largest = getLargest(10000)
-print(f"LARGEST:  ({len(largest)} entries)\n{largest}")
+    class TestCollatzMethods(unittest.TestCase):
+
+        def test_collatz_10(self):
+            self.assertEqual(
+                collatz(10),
+                [10,5,16,8,4,2,1]
+            )
+
+        def test_collatz_78(self):
+            self.assertEqual(
+                collatz(78),
+                [78,39,118,59,178,89,268,134,67,202,101,304,152,76,
+                 38,19,58,29,88,44,22,11,34,17,52,26,13,40,20,10,
+                 5,16,8,4,2,1]
+            )
+
+        def test_getLargest_100(self):
+            largest = getLargest(100)
+            self.assertEqual(
+                len(largest), 119
+            )
+            print(f"Largest ({len(largest)} values): {largest}")
+            
+
+    unittest.main()
