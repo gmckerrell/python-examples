b893f583b355d1e8e269bf68e7039e1a2cef29e1
Graeme McKerrell <31652935+gmckerrell@users.noreply.github.com>
2021-05-13 08:09:52 +0100

    Create rgb_template.py

---
+++ puzzles/rgb_template.py
@@ -0,0 +1,56 @@
+"""
+Given an array of strictly the characters 'R', 'G', and 'B',
+segregate the values of the array so that all the Rs come first,
+the Gs come second, and the Bs come last. You can only swap elements of
+the array.
+
+Do this in linear time and in-place.
+
+For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'],
+it should become ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
+"""
+def rgb(values):
+    """
+    This function takes an array of 'R', 'G', 'B' values and orders them
+    in linear time, in-situ by swapping elements.
+
+    values - the array to manipulate
+    """
+    pass
+
+if __name__ == '__main__':
+    import unittest
+    import time
+    import random
+
+    class TestRgb(unittest.TestCase):
+
+        def _check_values(self, value_string, expected_string):
+            values = list(value_string)
+            expected = list(expected_string)
+            rgb(values)
+            self.assertEqual(values, expected)
+
+        def _gen_random_values(self, size):
+            values = []
+            choices = "RGB"
+            for i in range(0, size):
+                values.append(
+                    random.choice(choices)
+                )
+            return values
+            
+        def test_example(self):
+            self._check_values("GBRRBRG", "RRRGGBB")
+
+        def test_timings(self):
+            print()
+            for size in (10, 100, 1000, 10000, 100000):
+                values = self._gen_random_values(size)
+                start = time.perf_counter()
+                rgb(values)
+                end = time.perf_counter()
+                delta = (end - start) * 1000
+                print(f"{size:10}: {delta:0.3f} ms [{delta/size:0.6f} ms/value]")
+            
+    unittest.main()
