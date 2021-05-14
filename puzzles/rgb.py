1f34fe19c8d10b57b959a2d502a316fb75e2d633
Graeme McKerrell <31652935+gmckerrell@users.noreply.github.com>
2021-05-14 10:37:09 +0100

    Update rgb.py

--- puzzles/rgb.py
+++ puzzles/rgb.py
@@ -9,6 +9,9 @@ Do this in linear time and in-place.
 For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'],
 it should become ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
 """
+def _swap(values, left, right):
+    values[left], values[right] = values[right], values[left]
+    
 def rgb(values):
     """
     This function takes an array of 'R', 'G', 'B' values and orders them
@@ -16,27 +19,28 @@ def rgb(values):
 
     values - the array to manipulate
     """
-    pos = 0
-    end = len(values)
-    while (pos < end):
+    # we track in the internal "green" zone
+    first_green_pos = 0
+    last_green_pos  = len(values) - 1
+    pos             = 0
+    while (pos <= last_green_pos):
         value = values[pos]
         if value == 'R':
-            # pop out the entry
-            values.pop(pos)
-            # put it at the beginning of the list
-            values.insert(0, value)
-            # move onto the next value
-            pos += 1
+            if pos > first_green_pos:   
+                # move this out of the green zone...
+                _swap(values, first_green_pos, pos)
+                # ... and move the start marker
+                first_green_pos += 1
+            else:
+                # move forward as we're not in the green zone yet
+                pos += 1
         elif value == 'B':
-            # pop out the entry
-            values.pop(pos)
-            # put it at the end of the list
-            values.insert(end, value)
-            # we no longer need to process this far
-            end -= 1
-            # we do not move forward as the next value is now here
+            # move this out of the green zone...
+            _swap(values, pos, last_green_pos)
+            # ... and move the end marker
+            last_green_pos -= 1
         else:
-            # nothing changes move onto next value
+            # continue through the green zone
             pos += 1
 
 if __name__ == '__main__':
@@ -55,15 +59,24 @@ if __name__ == '__main__':
         def test_example(self):
             self._check_values("GBRRBRG", "RRRGGBB")
 
+        def test_start_with_red(self):
+            self._check_values("RGBRRBRG", "RRRRGGBB")
+
+        def test_end_with_red(self):
+            self._check_values("GBRRBRGR", "RRRRGGBB")
+
+        def test_end_with_blue(self):
+            self._check_values("GBRRBRGB", "RRRGGBBB")
+
         def test_timings(self):
             print()
             random.seed("12345") # make this reproducible
-            for size in (10, 100, 1000, 10000, 100000):
-                values = random.choices("RGB", k = size)
+            for size in (10, 100, 1000, 10000, 100000, 1000000):
+                values = random.choices("RGB", k=size)
                 start = time.perf_counter()
                 rgb(values)
                 end = time.perf_counter()
                 delta = (end - start) * 1000
-                print(f"{size:10}: {delta:0.2f} ms [{delta/size:0.3f} ms/value]")
+                print(f"{size:10}: {delta:0.2f} ms [{1000 * delta/size:0.4f} us/value]")
             
     unittest.main()
