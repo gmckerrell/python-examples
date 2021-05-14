8ab8a8274706ad621367443309fb0f5a01694dca
Graeme McKerrell <31652935+gmckerrell@users.noreply.github.com>
2021-05-14 10:37:36 +0100

    Update rgb_template.py

--- puzzles/rgb_template.py
+++ puzzles/rgb_template.py
@@ -9,6 +9,9 @@ Do this in linear time and in-place.
 For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'],
 it should become ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
 """
+def _swap(values, left, right):
+    values[left], values[right] = values[right], values[left]
+    
 def rgb(values):
     """
     This function takes an array of 'R', 'G', 'B' values and orders them
@@ -16,7 +19,7 @@ def rgb(values):
 
     values - the array to manipulate
     """
-    pass
+    []
 
 if __name__ == '__main__':
     import unittest
@@ -34,15 +37,24 @@ if __name__ == '__main__':
         def test_example(self):
             self._check_values("GBRRBRG", "RRRGGBB")
 
+        def test_start_with_red(self):
+            self._check_values("RGBRRBRG", "RRRRGGBB")
+
+        def test_end_with_red(self):
+            self._check_values("GBRRBRGR", "RRRRGGBB")
+
+        def test_end_with_blue(self):
+            self._check_values("GBRRBRGB", "RRRGGBBB")
+
         def test_timings(self):
             print()
             random.seed("12345") # make this reproducible
-            for size in (10, 100, 1000, 10000, 100000):
-                values = random.choices("RGB", k = size)
+            for size in (10, 100, 1000, 10000, 100000, 1000000):
+                values = random.choices("RGB", k=size)
                 start = time.perf_counter()
                 rgb(values)
                 end = time.perf_counter()
                 delta = (end - start) * 1000
-                print(f"{size:10}: {delta:0.2f} ms [{delta/size:0.3f} ms/value]")
+                print(f"{size:10}: {delta:0.2f} ms [{1000 * delta/size:0.4f} us/value]")
             
     unittest.main()
